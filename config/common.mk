579408948770d98da5581b1c3ed38bb896b6c107
scott <scott.hart.bti@gmail.com>
2016-08-15 13:33:36 -0400

    Revert "Revert "config: Add ThemeManagerService to PRODUCT_PACKAGES""
    This reverts commit fe39b461d5dbe0811c8379d830c46b1708104668.

--- config/common.mk
+++ config/common.mk
@@ -117,7 +117,8 @@ PRODUCT_PACKAGES += \
     CMAudioService \
     Development \
     BluetoothExt \
-    Profiles
+    Profiles \
+    ThemeManagerService
 
 # DU Utils Library
 PRODUCT_PACKAGES += \
